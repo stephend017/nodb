9d22c83876f656391893973a6628732836020352
Stephen Davis <davi1138@purdue.edu>
2020-02-16 22:56:52 -0500

    (feature): Added documentation to NoDB wrapper

--- index.tsx
+++ index.tsx
@@ -2,29 +2,53 @@ import { NDBConfig } from "./src/config";
 import { NDBRecord } from "./src/record";
 import { createFile, modifyFile, getFile, deleteFile } from "./src/githubFileIO";
 
-export { NDBRecord, NDBConfig};
-export { NDBRecordKeyType, NDBAttribute }from './src/record';
+export { NDBRecord, NDBConfig };
+export { NDBRecordKeyType, NDBAttribute } from './src/record';
 
+/**
+ * JS Wrapper for NoDB
+ */
 export default class NoDB {
+
     private static config: NDBConfig;
 
+    /**
+     * Sets the settings used for NoDB
+     * @param config settings to use for NoDB
+     */
     public static Init(config: NDBConfig) {
         NoDB.config = config;
     }
 
-    public static Create<RecordType extends NDBRecord>(record: RecordType) {
+    /**
+     * Stores a new record in the database
+     * @param record new record to store in the database. 
+     */
+    public static Create<RecordType extends NDBRecord>(record: RecordType): void {
         createFile(NoDB.config, record);
     }
 
-    public static Modify<RecordType extends NDBRecord>(newRecord: RecordType) {
-        modifyFile(NoDB.config, newRecord);
+    /**
+     * Overwrites a record with the same key in the database
+     * @param record new record to store in the database
+     */
+    public static Modify<RecordType extends NDBRecord>(record: RecordType): void {
+        modifyFile(NoDB.config, record);
     }
 
-    public static async Get(record: NDBRecord) {
-        return await getFile(NoDB.config, record);
+    /**
+     * Returns the JSON object of the record being returned
+     * @param record type and key of record to get from the database
+     */
+    public static async Get(record: NDBRecord): Promise<any> {
+        return JSON.parse(await getFile(NoDB.config, record));
     }
 
-    public static Delete(record: NDBRecord) {
+    /**
+     * Deletes a record from the database
+     * @param record type and key of the record to delete from the database
+     */
+    public static Delete(record: NDBRecord): void {
         deleteFile(NoDB.config, record)
     }
 
