3f97c89c41f710022e27cced09e847d23713ea18
Stephen Davis <davi1138@purdue.edu>
2020-02-16 20:36:21 -0500

    (fix): removed old files

--- dist/src/config.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class NDBConfig {
-    readonly github_oauth_token: string;
-    readonly repositoryOwner: string;
-    readonly ownerEmail: string;
-    readonly repository: string;
-    constructor(github_oauth_token: string, repositoryOwner: string, ownerEmail: string, repository: string);
-}
