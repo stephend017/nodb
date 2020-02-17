3f89edfad166d13a99397d60b9dcb42c12a17dae
Stephen Davis <davi1138@purdue.edu>
2020-02-16 23:32:13 -0500

    (feature): Added documentation to NDBConfig

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,22 @@
 export class NDBConfig {
+    /**
+     * github oauth token. see [here](https://github.com/settings/tokens) for more information.
+     */
     public readonly github_oauth_token: string;
+
+    /**
+     * Github repository owner's username
+     */
     public readonly repositoryOwner: string;
+
+    /**
+     * Github repository owner's email
+     */
     public readonly ownerEmail: string;
+
+    /**
+     * Name of the github repository
+     */
     public readonly repository: string;
 
     constructor(github_oauth_token: string, repositoryOwner: string, ownerEmail: string, repository: string) {
